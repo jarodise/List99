b41fe99677e1b9ab9c3815b813de03df30ebf8c8
jarodise <jarodise@gmail.com>
2022-07-19 17:36:21 +0300

    comment

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ with open('list.txt', 'r') as f:
         f.writelines(lines)
         f.close()
 print(english)
-# 创建翻译目标
+# 创建翻译目标并翻译原始单词表
 translator = Translator()
 portuguese = translator.translate(english, dest="pt")
 portuguese = str(portuguese.text)
@@ -19,21 +19,21 @@ portuguese = str(portuguese.text)
 f = open("portuguese.txt","w+")
 f.writelines(portuguese)
 f.close()
+# 在生成的翻译文件每一行开头加入 "-" 
 with open('portuguese.txt', 'r') as f:
     lines = f.readlines()
     lines = ['- ' + line for line in lines]
     with open('portuguese.txt', 'w') as f:
         f.writelines(lines)
         f.close()
-#组合两个语言到一个语言#
+#组合两个语言到一个语言并构建logseq flashcard格式和tagging
 with open('english.txt') as f1, open('portuguese.txt') as f2, open('初来乍到99单词表-english-portuguese.txt', 'w') as out:
     for line1, line2 in zip(f1, f2):
         print(line1.rstrip(), " #card #english-portuguese", sep='', file=out)
         out.write('  ')
         out.write(line2)  
-#在文件顶部添加card query
+#在新文件顶部添加card query信息
 cards_query = '## {{cards [[english-portuguese]]}}\n\n'
-
 with open('初来乍到99单词表-english-portuguese.txt', 'r+') as file:
    content = file.read()
    file.seek(0)
